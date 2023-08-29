bb898732a9199369260ec1ef4693d77564bab98a
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-29 23:40:46 +0200

    Create contact mo component

---
+++ frontend/client/src/components/ContactMo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const ContactMo:React.FC = () => {
+  return (
+    <Link to='/' className='bg-[#000] px-5 py-2 text-[#fff] text-md'>Contact Mo</Link>
+  )
+}
+
+export default ContactMo;
