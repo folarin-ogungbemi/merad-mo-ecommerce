8d3368c19ba0e01f9e665b670a3d3f60964ae163
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-29 23:36:09 +0200

    create home route path

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
 
  const App:React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
   )
 }
 
