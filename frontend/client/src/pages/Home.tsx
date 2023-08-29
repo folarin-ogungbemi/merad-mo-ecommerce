23bc2998d845b52a5dc4cef365802dfe2c6d7a4e
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-29 23:43:54 +0200

    Create pages module

---
+++ frontend/client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Home:React.FC = () => {
+  return (
+    <Layout title='Merad & Mo | Home' content='About Merad & Mo'>
+        <h2>Home </h2>
+    </Layout>
+  )
+}
+
+export default Home;
