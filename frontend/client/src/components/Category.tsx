e91e1a07d8d5142bd81421586b2aea4b65a43285
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-31 21:10:45 +0200

    Modify category display

--- frontend/client/src/components/Category.tsx
+++ frontend/client/src/components/Category.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Category:React.FC = () => {
-    const categories = ['African special', 'corporate wears', 'casual wears', 'summer wears']
+
+    const categories = ['african special', 'corporate wears', 'casual wears', 'summer wears']
   return (
-    <div className='bg-[#f5eef8] py-2 border-t-[1px] border-b-gray-200 transition-all delay-400 duration-500'>
+    <div className='bg-[#f5eef8] py-2 border-t-[1px] border-t-gray-200 mx-auto fixed w-full'>
         <nav className='flex space-x-5 container mx-auto px-4'>
             {categories.map((category, index) =>
             <Link key={index} to='/' className='hover:bg-slate-200 py-1 px-3 rounded-full'>{category}</Link>
@@ -14,4 +14,4 @@ const Category:React.FC = () => {
   )
 }
 
-export default Category
+export default Category;
--- frontend/client/src/components/Header.tsx
+++ frontend/client/src/components/Header.tsx
@@ -11,6 +11,10 @@ import Category from './Category'
 const Header:React.FC = () => {
     const [showCategory, setShowCategory] = React.useState<boolean>(false)
 
+    const handleshowCategory = () =>{
+        setShowCategory(prevState => !prevState)
+    }
+
   return (
     <>
         <header className="w-full h-20 bg-white sticky top-0 z-40 border-b-[1px] border-b-gray-200">
@@ -23,13 +27,19 @@ const Header:React.FC = () => {
             </nav>
         </header>
         <nav className='w-full bg-[#f5eef8]'>
-            <div className='container h-20 mx-auto grid grid-cols-3 content-center px-4'>
-                <span className='flex text-[#000] my-auto cursor-pointer' onClick={()=>setShowCategory(prevState => !prevState)}>{!showCategory ? <RxHamburgerMenu className='my-auto mr-2'/> : <MdClose className='my-auto mr-2'/>} Shop by Category</span>
+            <div className='container h-20 mx-auto flex justify-between content-center px-4'>
+                <span 
+                    className='flex text-[#000] my-auto cursor-pointer text-sm' 
+                    onClick={handleshowCategory}>
+                    {!showCategory ? <RxHamburgerMenu className='my-auto mr-2'/>
+                    : <MdClose className='my-auto mr-2'/>}
+                     Shop by Category
+                </span>
                 <SearchBar/>
                 <Checker/>
             </div>
         </nav>
-        {showCategory ? <span className='block'><Category/></span> : <span className='hidden'><Category/></span>}
+        {showCategory && <Category/>}
     </>
   )
 }
