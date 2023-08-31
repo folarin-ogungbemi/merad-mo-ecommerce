9a5c700907782447c7c88fb87c662b8a00f1ce57
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-08-31 23:20:41 +0200

    create animation keyframes

--- frontend/client/tailwind.config.js
+++ frontend/client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+      'fade-in': {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '100' },
+      },
+    },
+    animation: {
+      'bg-opacity': 'fade-in 1s ease-in-out', // This maps to the 'fade-in' keyframe
+    },
+      textColor:{
+        skin:{
+          text_col:'var(--text-col)',
+          text_col_hover:'var(--text-col-hover)',
+        }
+    },
   },
+},
   plugins: [],
 }
