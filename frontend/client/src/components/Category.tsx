4f2dba56f1122b71d72e54920d4dddaf2b1dae79
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-09-02 13:12:28 +0200

    Add variable color

--- frontend/client/src/components/Category.tsx
+++ frontend/client/src/components/Category.tsx
@@ -8,10 +8,10 @@ const Category:React.FC<IProps> = ({setShowCategory}) => {
 
     const categories = ['african special', 'corporate wears', 'casual wears', 'summer wears']
   return (
-    <ul className='bg-[#f5eef8] border-t-[1px] border-t-gray-200 mx-auto fixed w-full' onMouseLeave={()=>setShowCategory(false)}>
-        <li className='flex space-x-3 container mx-auto px-4 text-skin-text_col text-sm capitalize'>
+    <ul className='bg-skin-pry_bg border-t-[1px] border-t-skin-section_divide mx-auto fixed w-full z-40' onMouseLeave={()=>setShowCategory(false)}>
+        <li className='flex space-x-3 container mx-auto px-4 text-sm capitalize'>
             {categories.map((category, index) =>
-            <Link key={index} to='/' className='py-3 rounded-full animate-bg-opacity nav-links font-normal hover:text-skin-text_col_hover hover:bg-opacity-100 transition-opacity duration-500 ease-in-out'>{category}</Link>
+            <Link key={index} to='/' className='py-3 rounded-full text-skin-links hover:text-skin-links_hover animate-bg-opacity nav-links font-normal hover:bg-opacity-100 transition-opacity duration-500 ease-in-out'>{category}</Link>
             )}
         </li>
     </ul>
