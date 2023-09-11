09b2ba6b19f9cf7130e8e1b346ae4732936bbde6
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-09-11 17:46:11 +0200

    Style Navlinks

--- frontend/client/src/components/NavLinks.tsx
+++ frontend/client/src/components/NavLinks.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const NavLinks:React.FC = () => {
 
-    const NavLinks = ['home', 'products', 'about', 'blog']
+    const NavLinks = ['home', 'shop', 'about', 'contact']
 
     return (
-        <ul className='flex space-x-5'>
+        <ul className='flex gap-5 pr-4'>
             {NavLinks.map((nav, navIndex) =>
-            <li className='selection:text-sm text-skin-links hover:text-skin-links_hover capitalize nav-links py-[0.5px]'>
-                <Link key={navIndex} to={nav === 'home' ? '/' : nav}>{nav}</Link>
+            <li key={navIndex} className='text-skin-links hover:text-skin-links_hover transition-all ease-in-out delay-300 duration-500 capitalize w-10'>
+                <Link to={nav === 'home' ? '/' : nav} className='w-fit nav-links hover:font-medium mx-auto py-2'>{nav}</Link>
             </li>)}
         </ul>
     )
