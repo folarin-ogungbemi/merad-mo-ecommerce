b5751fb6c7412ff4db1bb755b9bb1aa7ef59c5cd
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-09-12 11:49:58 +0200

    Route products page

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
+import Products from './pages/Products'
 
  const App:React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/shop' element={<Products />}/>
       </Routes>
     </Router>
   )
